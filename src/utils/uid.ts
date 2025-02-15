5447858a325fe89983852edd6e54763908b264fd
Zach Mueller <zmueller@amazon.com>
2025-02-16 09:31:41 +1300

    Fix spaces to tabs in uid code

--- src/utils/uid.ts
+++ src/utils/uid.ts
@@ -1,23 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export function generateUID(): string {
-  const u = uuidv4();
-  const hexlist = '0123456789abcdef';
-  const b64list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-  let s = u.replace(/[^0-9a-f]/ig, '').toLowerCase();
-  s += '0';
-  let a, p, q;
-  let r = '';
-  let i = 0;
-  while (i < 33) {
-    a = (hexlist.indexOf(s.charAt(i++)) << 8) |
-      (hexlist.indexOf(s.charAt(i++)) << 4) |
-      (hexlist.indexOf(s.charAt(i++)));
+	const u = uuidv4();
+	const hexlist = '0123456789abcdef';
+	const b64list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+	let s = u.replace(/[^0-9a-f]/ig, '').toLowerCase();
+	s += '0';
+	let a, p, q;
+	let r = '';
+	let i = 0;
+	while (i < 33) {
+		a = (hexlist.indexOf(s.charAt(i++)) << 8) |
+			(hexlist.indexOf(s.charAt(i++)) << 4) |
+			(hexlist.indexOf(s.charAt(i++)));
 
-    p = a >> 6;
-    q = a & 63;
+		p = a >> 6;
+		q = a & 63;
 
-    r += b64list.charAt(p) + b64list.charAt(q);
-  }
-  return r.replace('/', '_');
+		r += b64list.charAt(p) + b64list.charAt(q);
+	}
+	return r.replace('/', '_');
 }
